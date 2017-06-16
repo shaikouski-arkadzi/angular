4255c5334b5db2696c8a420a87482980d3821b57
Arkadzi Shaikouski <ShaikouskiA@iba>
2017-06-16 18:16:17 +0300

    Add some knowledge: - Directives - ngIf else - ngStyle -ngClass -ngFor Add some comments Finish section

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // common selector
   //selector: '[app-servers]',
+  // selector like a class
   selector: '.app-servers',
+  // template in a few lines
   // template: `
   //   <app-server></app-server>
   //   <app-server></app-server>
@@ -14,6 +17,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
+  servers = []
 
   constructor() { 
     setTimeout(() => {
@@ -25,11 +30,12 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
-    //this.serverName = <HTMLInputElement>event.target.value;
   }
 }
